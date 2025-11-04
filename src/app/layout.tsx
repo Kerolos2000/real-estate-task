8fcf6411709b2afff0b63397827430f801c25d48
Kerolos2000 <kero.magdy2000@gmail.com>
2025-11-04 22:23:14 +0200

    feat: 🎸 init app

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter-sans",
+});
+
+export const metadata: Metadata = {
+  title: "Real Estate Portal",
+  description: "Browse and explore real estate properties",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
